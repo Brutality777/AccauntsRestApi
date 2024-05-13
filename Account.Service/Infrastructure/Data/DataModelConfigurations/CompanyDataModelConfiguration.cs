4c50953b70cdfba1015f465cb070731e6146f904
Matvey <curtev.2006@gmail.com>
2024-05-13 13:29:50 +0300

    FIx the cascade drop

--- Account.Service/Infrastructure/Data/DataModelConfigurations/CompanyDataModelConfiguration.cs
+++ Account.Service/Infrastructure/Data/DataModelConfigurations/CompanyDataModelConfiguration.cs
@@ -17,8 +17,8 @@ namespace Account.Service.Infrastructure.Data.DataModelConfigurations
             builder.HasIndex(x => x.Name).IsUnique();
             builder.Property(x => x.Description).IsRequired().HasMaxLength(400);
             
-            builder.HasMany(x => x.Workers).WithMany(x => x.Company).UsingEntity<CompanyAccountRelationDataModel>(mg => mg.HasOne(prop => prop.Account).WithMany().HasForeignKey(x => x.AccountDataModelId),
-                mg => mg.HasOne(x => x.Company).WithMany().HasForeignKey(prop => prop.CompanyDataModelId),
+            builder.HasMany(x => x.Workers).WithMany(x => x.Company).UsingEntity<CompanyAccountRelationDataModel>(mg => mg.HasOne(prop => prop.Account).WithMany().HasForeignKey(x => x.AccountDataModelId).OnDelete(DeleteBehavior.Restrict),
+                mg => mg.HasOne(x => x.Company).WithMany().HasForeignKey(prop => prop.CompanyDataModelId).OnDelete(DeleteBehavior.Restrict),
                 mg =>
                 {
                     mg.HasKey(prop => new { prop.AccountDataModelId, prop.CompanyDataModelId });
